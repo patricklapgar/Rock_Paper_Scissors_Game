4d1499bccbc2648c70ad7efa69be59828c4d303a
patricklapgar <patricklapgardeveloper@gmail.com>
2020-06-13 22:48:32 -0500

    finished building version 2.

--- rps_version_2.py
+++ rps_version_2.py
@@ -11,21 +11,25 @@ print("Paper...")
 print("Scissors...")
 
 player1 = input("Player 1, make your move:\n")
+print("**** NO CHEATING ****\n\n" * 20)
 player2 = input("Player 2, make your move:\n")
 
-if player1 == "rock" and player2 == "scissors":
-    print("player 1 wins!!")
-elif player1 == "rock" and player2 == "paper":
-    print("player 2 wins!!")
-elif player1 == "paper" and player2 == "rock":
-    print("player 1 wins!!")
-elif player1 == "paper" and player2 == "scissors":
-    print("player 2 wins!!")
-elif player1 == "scissors" and player2 == "rock":
-    print("player 2 wins!!")
-elif player1 == "scissors" and player2 == "paper":
-    print("player 1 wins!!")
-elif player1 == player2:
-    print("Tie game!!")
+if player1 == player2:
+        print("Tie game!!")
+elif player1 == "rock":
+    if player2 == "scissors":
+            print("player 1 wins!!")
+    elif player2 == "paper":
+        print("player 2 wins!!")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("player 1 wins!!")
+    elif player2 == "scissors":
+        print("player 2 wins!!")
+elif player1 == "scissors":
+    if player2 == "rock":
+        print("player 2 wins!!")
+    elif player2 == "paper":
+        print("player 1 wins!!")
 else:
     print("something went wrong, please enter a choice")
