ed27f9f08067d2a5a58a8f1834439de66f1b6fec
patricklapgar <patricklapgardeveloper@gmail.com>
2020-06-13 19:03:40 -0500

    created a conditional statement

--- rps_version_1.py
+++ rps_version_1.py
@@ -11,3 +11,16 @@ print("Scissors...")
 
 player1 = input("Player 1, make your move:\n ")
 player2 = input("Player 2, make your move:\n ")
+
+if player1 == "rock" and player2 == "scissors":
+    print("player 1 wins!!")
+elif player1 == "rock" and player2 == "paper":
+    print("player 2 wins!!")
+elif player1 == "paper" and player2 == "rock":
+    print("player 1 wins")
+elif player1 == "paper" and player2 == "scissors":
+    print("player 2 wins")
+elif player1 == player2:
+    print("Tie game!!")
+else:
+    print("something went wrong, please enter a choice")
