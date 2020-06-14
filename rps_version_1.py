10a07d3990827fe72a9084250a318b79f6477e16
patricklapgar <patricklapgardeveloper@gmail.com>
2020-06-13 19:00:05 -0500

    Began building version of rps game. Added input statements only.

---
+++ rps_version_1.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors Game
+# There are two versions of this game that will be created.
+# The first will be basic version where two players enter their choice and 
+# only one will win
+
+# The second version will put the player against a computer-generated answer
+
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player1 = input("Player 1, make your move:\n ")
+player2 = input("Player 2, make your move:\n ")
