2730d94fac73b48028b62a1bf52aa5ca04c1fcf3
patricklapgar <patricklapgardeveloper@gmail.com>
2020-06-13 19:08:52 -0500

    Finished conditional statement. I will now refactor

--- rps_version_1.py
+++ rps_version_1.py
@@ -9,17 +9,21 @@ print("Rock...")
 print("Paper...")
 print("Scissors...")
 
-player1 = input("Player 1, make your move:\n ")
-player2 = input("Player 2, make your move:\n ")
+player1 = input("Player 1, make your move:\n")
+player2 = input("Player 2, make your move:\n")
 
 if player1 == "rock" and player2 == "scissors":
     print("player 1 wins!!")
 elif player1 == "rock" and player2 == "paper":
     print("player 2 wins!!")
 elif player1 == "paper" and player2 == "rock":
-    print("player 1 wins")
+    print("player 1 wins!!")
 elif player1 == "paper" and player2 == "scissors":
-    print("player 2 wins")
+    print("player 2 wins!!")
+elif player1 == "scissors" and player2 == "rock":
+    print("player 2 wins!!")
+elif player1 == "scissors" and player2 == "paper":
+    print("player 1 wins!!")
 elif player1 == player2:
     print("Tie game!!")
 else:
